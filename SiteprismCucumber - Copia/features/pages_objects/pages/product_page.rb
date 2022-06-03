a95c243397dda12780bd22af155f8d7e7b279bb2
silva1996 <marcosj.antonio3@gmail.com>
2022-06-02 22:42:55 -0300

    inclusão da aula dia 7

--- SiteprismCucumber - Copia/features/pages_objects/pages/product_page.rb
+++
@@ -1,8 +0,0 @@
-module Pages
-    class ProductPage < SitePrism::Page
-        set_url '/'
-
-        elements :colors, '.productColor'
-        element :main_image, '#mainImg'
-    end
-end
--- SiteprismCucumber - Copia/features/pages_objects/sections/product.rb
+++
@@ -1,7 +0,0 @@
-module Sections
-    class Products < SitePrism::Section
-        element :name, 'a.productName'
-        element :price, 'a.productPrice'
-        element :image, 'img.imgProduct'
-    end
-end
